e834a9f8202a49ba8a2e4ec60d77c70101d40567
oniolalekan <olalekan.joseph@gmail.com>
2023-02-06 08:56:29 -0500

    refactor code

--- metric_extraction.py
+++ metric_extraction.py
@@ -4,7 +4,6 @@ from pandas import to_datetime
 
 
 def metrics_extract(data):
-
     site_id = data['Site ID'].tolist()
     cleaned_site_id = [x for x in site_id if str(x) != 'nan']
 
@@ -20,52 +19,52 @@ def metrics_extract(data):
             date_viewed = [to_datetime(w) for w in df_dataset_list if type(w) == datetime]
             day_viewed = [to_datetime(w).day for w in df_dataset_list if type(w) == datetime]
             if "Page Views" in i:
-                page_view = [p for p in df_dataset_list if type(p) == int]
-                combined_page_view = pd.DataFrame(list(zip(day_viewed, date_viewed, cleaned_site_id, page_view)),
-                                                  columns=['Day of Month', 'Date', 'Site ID', 'Page Views'])
-                old_df_pv = pd.concat([combined_page_view, df_page_view])
+                header_type = "Page Views"
+                result = transpose_data(df_dataset_list, day_viewed, date_viewed, cleaned_site_id, header_type)
+                old_df_pv = pd.concat([result, df_page_view])
                 df_page_view = old_df_pv
             elif "Unique Visitors" in i:
-                uniq_visitors = [u for u in df_dataset_list if type(u) == int]
-                combined_unique_visitors = pd.DataFrame(list(zip(day_viewed, date_viewed, cleaned_site_id, uniq_visitors)),
-                                                        columns=['Day of Month', 'Date', 'Site ID', 'Unique Visitors'])
-                old_df_uv = pd.concat([combined_unique_visitors, df_uniq_visitor])
+                header_type = "Unique Visitors"
+                result = transpose_data(df_dataset_list, day_viewed, date_viewed, cleaned_site_id, header_type)
+                old_df_uv = pd.concat([result, df_uniq_visitor])
                 df_uniq_visitor = old_df_uv
             elif "Total Time Spent" in i:
-                tt_spent = [t for t in df_dataset_list if type(t) == int]
-                combined_time_spent = pd.DataFrame(list(zip(day_viewed, date_viewed, cleaned_site_id, tt_spent)),
-                                                   columns=['Day of Month', 'Date', 'Site ID', 'Total Time Spent'])
-                old_df_ttt = pd.concat([combined_time_spent, df_total_time_spent])
+                header_type = "Total Time Spent"
+                result = transpose_data(df_dataset_list, day_viewed, date_viewed, cleaned_site_id, header_type)
+                old_df_ttt = pd.concat([result, df_total_time_spent])
                 df_total_time_spent = old_df_ttt
             elif "Visits" in i:
-                visits = [v for v in df_dataset_list if type(v) == int]
-                combined_visit = pd.DataFrame(list(zip(day_viewed, date_viewed, cleaned_site_id, visits)),
-                                              columns=['Day of Month', 'Date', 'Site ID', 'Visits'])
-                old_df_vst = pd.concat([combined_visit, df_visit])
+                header_type = "Visits"
+                result = transpose_data(df_dataset_list, day_viewed, date_viewed, cleaned_site_id, header_type)
+                old_df_vst = pd.concat([result, df_visit])
                 df_visit = old_df_vst
             else:
-                avg_time_spent = [a for a in df_dataset_list if type(a) == float]
-                combined_avg_time_spent = pd.DataFrame(list(zip(day_viewed, date_viewed, cleaned_site_id, avg_time_spent)),
-                                                       columns=['Day of Month', 'Date', 'Site ID',
-                                                                'Average Time Spent on Site'])
+                # transpose the average time spent here
+                avg_time_spent = [a for a in df_dataset_list if isinstance(a, (int, float))]
+                combined_avg_time_spent = pd.DataFrame(
+                    list(zip(day_viewed, date_viewed, cleaned_site_id, avg_time_spent)),
+                    columns=['Day of Month', 'Date', 'Site ID',
+                             'Average Time Spent on Site'])
                 old_df_avts = pd.concat([combined_avg_time_spent, df_avg_time_spent])
                 df_avg_time_spent = old_df_avts
     except Exception as e:
         print(f"Unexpected {e=}, {type(e)=}")
         raise
 
-    result_page_view = df_page_view.sort_values(['Site ID', 'Day of Month'])
-    result_uniq_visitors = df_uniq_visitor.sort_values(['Site ID', 'Day of Month'])
-    result_total_time_spent = df_total_time_spent.sort_values(['Site ID', 'Day of Month'])
-    result_visits = df_visit.sort_values(['Site ID', 'Day of Month'])
-    result_average_time_spent = df_avg_time_spent.sort_values(['Site ID', 'Day of Month'])
-
-    result_1 = result_page_view.merge(result_uniq_visitors, how='outer')
-    result_2 = result_1.merge(result_total_time_spent, how='outer')
-    result_3 = result_2.merge(result_visits, how='outer')
-    result = result_3.merge(result_average_time_spent, how='outer')
+    result_1 = df_page_view.merge(df_uniq_visitor, how='outer')
+    result_2 = result_1.merge(df_total_time_spent, how='outer')
+    result_3 = result_2.merge(df_visit, how='outer')
+    result = result_3.merge(df_avg_time_spent, how='outer')
+    result = result.sort_values(by=['Site ID', 'Day of Month'], ascending=True)
     result.to_csv('data.csv', index=False)
     return result.shape
+
+
+def transpose_data(df_dataset_list, day_viewed, date_viewed, cleaned_site_id, header_type):
+    data_per_header = [p for p in df_dataset_list if isinstance(p, int)]
+    combined_data = pd.DataFrame(list(zip(day_viewed, date_viewed, cleaned_site_id, data_per_header)),
+                                 columns=['Day of Month', 'Date', 'Site ID', header_type])
+    return combined_data
 
 
 if __name__ == '__main__':
